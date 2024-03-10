c44f7665312894dc1b025b6ffa547b8db0f19d2c
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2024-03-10 12:44:06 +0800

    add ep 9

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,12 +1,12 @@
 export const metadata = {
-  title: 'Home - Podcast',
-  description: 'Page description',
+  title: "Code & Cash - Podcast",
+  description: "Les développeurs aussi peuvent faire du cash.",
 };
 
-import Cta from '@/components/cta';
-import Hero from '@/components/hero';
-import Hosts from '@/components/hosts';
-import Podcasts from '@/components/podcasts';
+import Cta from "@/components/cta";
+import Hero from "@/components/hero";
+import Hosts from "@/components/hosts";
+import Podcasts from "@/components/podcasts";
 
 export default function Home() {
   return (
